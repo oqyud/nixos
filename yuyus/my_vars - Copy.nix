baf35861d56c92f5b2af21dabb88d8f4e5886a94
oqyude <go.bin043120@gmail.com>
2024-11-24 19:03:00 +0300

    123

---
+++ yuyus/my_vars - Copy.nix
@@ -0,0 +1,16 @@
+rec {
+  this-host = "yuyus";
+  dirs = rec {
+    user-home = "/home/${this-host}";
+    home = "/mnt/server";
+    structure = "${home}/Structure";
+    sync = "${home}/Sync";
+    shared = "${structure}/Shared";
+    storage = "${shared}/Storage";
+    deploy = "${shared}/Deploy";
+    user = "${structure}/User";
+    programs = "${storage}/Programs";
+    settings = "${storage}/Settings";
+    nixos = "${deploy}/NixOS";
+  };
+}
